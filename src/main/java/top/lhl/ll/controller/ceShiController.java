b1cd80eb707df732ba638f6347f62e4e04063805
lhl <2115056561@qq.com>
2022-09-06 00:27:18 +0800

    新增了ceShiController

---
+++ src/main/java/top/lhl/ll/controller/ceShiController.java
@@ -0,0 +1,13 @@
+package top.lhl.ll.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author: LHL
+ * Date  : 2022/9/6 0:22
+ */
+@RestController
+public class ceShiController {
+}
